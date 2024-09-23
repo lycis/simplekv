84628c505482eb695a53ff62f8805f33574aa27f
Daniel Eder <daniel@deder.at>
2024-09-23 13:05:43 +0200

    Refactor logging function and add timestamp and log level information

--- main.c
+++ main.c
@@ -2,24 +2,53 @@
 #include <WinSock2.h>
 #include <time.h>
 
-void logMessage(int level, const char* message) {
+enum LogLevel {
+    INFO = 0,
+    WARN = 1,
+    ERR = 2
+};
+
+void getCurrentTimeString(char* buffer) {
+    time_t t = time(NULL);
+    struct tm buf;
+    char timeStamp[26];
+    errno_t err = localtime_s(&buf, &t);    
+    asctime_s(timeStamp, sizeof timeStamp, &buf);
+    timeStamp[24] = '\0';
+    strcpy_s(buffer, 25, timeStamp);
+    return;
+}
+
+const char* getLogLevelAsStr(enum LogLevel l) {
+    switch(l) {
+        case INFO:
+            return "INFO";
+        case WARN:
+            return "WARN";
+        case ERR:
+            return "ERR";
+        default:
+            return "UNKNOWN";
+    }
+}
+
+void logMessage(enum LogLevel lvl, const char* message) {
     #ifdef WIN64
-        time_t t = time(NULL);
-        struct tm buf;
-        char str[26];
-        errno_t err = localtime_s(&buf, &t);    
-        asctime_s(str, sizeof str, &buf);
-        str[24] = '\0';
-        printf("%s: %s\n", str, message);
+        char timeStampStr[26];
+        getCurrentTimeString(timeStampStr);
+        printf("%s - %s - %s\n", timeStampStr, getLogLevelAsStr(lvl), message);
     #else
-        printf("%s\n", message);
+        printf("%s - %s\n", lvlStr, message);
     #endif
 }
 
 int main() {
-    logMessage(0, "Starting ckvstr.");
+    logMessage(INFO, "Starting server.");
 
     //int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    logMessage(INFO, "Server shutdown complete.");
+
 
     return 0;
 }
