ab67ed1adc09883185e3a6da16385a0d92f553cc
Daniel Eder <daniel@deder.at>
2024-09-24 16:49:39 +0200

    build client and server

--- ci-build.sh
+++ ci-build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcc -fdiagnostics-color=always -g server.c -o server.exe -lws2_32
+mkdir dist
+gcc -fdiagnostics-color=always -g server.c -o dist/server.exe -lws2_32
+gcc -fdiagnostics-color=always -g client.c -o dist/client.exe -lws2_32
