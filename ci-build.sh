e30dab2a622628e8ef904d19061c342ec893b8cd
Daniel Eder <daniel@deder.at>
2024-09-24 16:55:22 +0200

    extended ci script

--- ci-build.sh
+++ ci-build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 mkdir dist
+
+# Compile the server
 gcc -fdiagnostics-color=always -g server.c -o dist/server.exe -lws2_32
+if [ $? -ne 0 ]; then
+    echo "Compilation of server.c failed"
+    exit 1
+fi
+
+# Compile the client
 gcc -fdiagnostics-color=always -g client.c -o dist/client.exe -lws2_32
+if [ $? -ne 0 ]; then
+    echo "Compilation of client.c failed"
+    exit 1
+fi
+
+echo "Compilation successful!"
