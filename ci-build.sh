253a802e8500f33423f2018455fdc16ce74cd501
Daniel Eder <daniel@deder.at>
2024-09-25 14:20:59 +0200

    added unit tests to build

--- ci-build.sh
+++ ci-build.sh
@@ -16,3 +16,16 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Compilation successful!"
+
+# Run server tests
+gcc -fdiagnostics-color=always -DWIN64 -DUNIT_TEST -g server.c -o dist/server_test.exe -lws2_32
+if [ $? -ne 0 ]; then
+    echo "Compilation of server unit tests failed"
+    exit 1
+fi
+
+./dist/server_test.exe
+if [ $? -ne 0 ]; then
+    echo "Server unit tests failed"
+    exit 1
+fi
